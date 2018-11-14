d6f39af7c9ff1506d013a6950034757213f2b964
Imad Ahmed <erimadahmad@gmail.com>
2018-11-14 17:40:23 +0530

    v1

---
+++ app.js
@@ -0,0 +1,15 @@
+var subdomain = require('express-subdomain');
+var express = require('express');
+var apiRoutes = require('./routes/subdomains');
+var app = express();
+ 
+// *** Code examples below go here! ***
+ 
+// example.com
+app.get('/', function(req, res) {
+    res.send('Homepage');
+});
+
+app.use(subdomain('api', apiRoutes));
+
+app.listen(3000);
